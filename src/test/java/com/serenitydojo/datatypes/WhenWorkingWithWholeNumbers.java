ed5b1ba43582cf2d915b8bceddda828b5916bd9d
Karina Kirpicheva <karina@sayprimer.com>
2024-05-16 18:10:05 +0400

    Exercise 1

--- src/test/java/com/serenitydojo/datatypes/WhenWorkingWithWholeNumbers.java
+++ src/test/java/com/serenitydojo/datatypes/WhenWorkingWithWholeNumbers.java
@@ -9,7 +9,7 @@ public class WhenWorkingWithWholeNumbers {
     @Test
     public void addingNumbersTogether() {
         int initialYear = 1985;
-        int targetYear = 0;
+        int targetYear = 2015;
 
         // TODO: create a new int variable called timeJump and assign it a value
         //  Next, add this variable to initialYear and assign the result to targetYear, so that targetYear is equal to 2015
